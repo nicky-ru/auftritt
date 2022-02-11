3951ac2cf0a8a17d4e0fc86ff31bce8703d4317b
Nicky Ru <nikitkaruban@gmail.com>
2022-02-12 00:32:17 +0800

    Update publicConfig.ts

--- config/publicConfig.ts
+++ config/publicConfig.ts
@@ -1,4 +1,4 @@
 export const publicConfig = {
-    HASURA_GRAPHQL_API: "https://halodashv2.hasura.app/v1/graphql",
+    HASURA_GRAPHQL_API: "https://halodashv2.hasura.app/v1/graphql",  // put your hasura api here
     HASURA_ADMIN_SECRET: process.env.HASURA_ADMIN_SECRET
 }
